1ea4ce55d3bab220494b388072c0b0837eb96929
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-08-10 01:48:47 -0400

    boton para multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,11 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
-
+const fom = document.querySelector("multiplicar-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -12,4 +13,14 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  
+});
+fom.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
